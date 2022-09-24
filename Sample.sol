2af8d3979e704eb5cf5232a3b8032861ed535a17
Aditya K <adityakaklij11@gmail.com>
2022-09-24 10:54:17 +0530

    Create Sample.sol

---
+++ Sample.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+
+// Solidity Version. 
+pragma solidity ^0.8.4;
+
+
+// Initialising Smart Contract. 
+contract Sample {
+
+    uint public num = 10;
+    string public data = "This is String";
+    address public user;
+
+    //Array
+    uint[] public array;
+
+    // Calling function to change the "data" variable. 
+    function setData(string memory _str) public {
+        data = _str;
+    }
+
+    //Calling function to add data into array. 
+    function insertIntoArray(uint _num) public {
+        array.push(_num);
+    }
+
+    // Assigning the value to the address.
+    function setUsr() public{
+        user = msg.sender;
+    }
+}
